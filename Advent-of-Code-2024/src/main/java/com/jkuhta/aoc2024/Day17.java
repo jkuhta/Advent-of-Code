5ebfcad86223be56d7aac19e2612e0c7750f443f
Jan Kuhta <jan.kuhta@gmail.com>
2025-01-03 22:47:42 +0100

    add: aoc2024 day17 solution

--- Advent-of-Code-2024/src/main/java/com/jkuhta/aoc2024/Day17.java
+++ Advent-of-Code-2024/src/main/java/com/jkuhta/aoc2024/Day17.java
@@ -18,13 +18,14 @@ public class Day17 {
         String[] registerLines = input.split("\n\n")[0].split("\n");
         String[] programLine = input.split("\n\n")[1].split(": ")[1].split(",");
 
-        long A = Long.parseLong(registerLines[0].split(": ")[1]);
-        int B = Integer.parseInt(registerLines[1].split(": ")[1]);
-        int C = Integer.parseInt(registerLines[2].split(": ")[1]);
+        Map<Character, Long> registers = new HashMap<>();
+        registers.put('A', Long.parseLong(registerLines[0].split(": ")[1]));
+        registers.put('B', Long.parseLong(registerLines[1].split(": ")[1]));
+        registers.put('C', Long.parseLong(registerLines[2].split(": ")[1]));
 
         List<Integer> program = Arrays.stream(programLine).map(Integer::parseInt).toList();
 
-        return runProgram(program, A, B, C).stream()
+        return runProgram(program, registers).stream()
                 .map(String::valueOf)
                 .collect(Collectors.joining(","));
     }
@@ -33,15 +34,23 @@ public class Day17 {
 
         String[] programLine = input.split("\n\n")[1].split(": ")[1].split(",");
 
+        Map<Character, Long> registers = new HashMap<>();
+        registers.put('A', 0L);
+        registers.put('B', 0L);
+        registers.put('C', 0L);
         List<Integer> program = Arrays.stream(programLine).map(Integer::parseInt).toList();
 
-        return selfReplicator(program);
+        return findRegister(program, registers, program.size() - 1);
     }
 
-    private static List<Integer> runProgram(List<Integer> program, long A, long B, long C) {
+    private static List<Integer> runProgram(List<Integer> program, Map<Character, Long> registers) {
 
         int position = 0;
         List<Integer> outputs = new ArrayList<>();
+
+        long A = registers.get('A');
+        long B = registers.get('B');
+        long C = registers.get('C');
 
         while (position < program.size()) {
 
@@ -81,24 +90,27 @@ public class Day17 {
         return outputs;
     }
 
-    public static long selfReplicator(List<Integer> program) {
-        SortedSet<Long> candidates = new TreeSet<>();
-        candidates.add(0L);
-        for (int i = 0; i < program.size(); i++) {
-            var lastInstruction = program.get(program.size() - i - 1);
-            SortedSet<Long> newCandidates = new TreeSet<>();
-            for (var old : candidates) {
-                long candidate = old << 3;
-                for (int j = 0; j < 8; j++) {
-                    if (Objects.equals(runProgram(program, candidate, 0, 0).getFirst(), lastInstruction)) {
-                        newCandidates.add(candidate);
-                    }
-                    candidate++;
+    public static long findRegister(List<Integer> program, Map<Character, Long> registers, int ix) {
+        for (int i = 0; i < 8; i++) {
+            Map<Character, Long> newRegisters = new HashMap<>(registers);
+            newRegisters.put('A', newRegisters.get('A') + i);
+
+            List<Integer> result = runProgram(program, newRegisters);
+            List<Integer> expectedSegment = program.subList(ix, program.size());
+
+            if (result.toString().equals(expectedSegment.toString())) {
+                if (ix == 0) {
+                    return newRegisters.get('A');
+                }
+
+                newRegisters.put('A', newRegisters.get('A') * 8);
+                long found = findRegister(program, newRegisters, ix - 1);
+                if (found != -1) {
+                    return found;
                 }
             }
-            candidates = newCandidates;
         }
-        return candidates.first();
+        return -1;
     }
 
     private static long combo(int operand, long A, long B, long C) {
